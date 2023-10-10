d3fd5870f9fe7b8bd3bfd36ddcdee9485c539090
msebastian22 <matthew.seb15@gmail.com>
2023-10-10 11:59:16 -0400

    Adding get function

---
+++ queue/queueGet.c
@@ -0,0 +1,104 @@
+/* 
+ * queue.c --- 
+ * 
+ * Author: Emma Vejcik
+ * Created: 10-10-2023
+ * Version: 1.0
+ * 
+ * Description: 
+ * 
+ */
+
+typedef struct element{
+	void data;
+	struct *element next;
+} element_t;
+
+typedef void{                                                           
+  element_t *front;
+	element_t *back;                                                         
+} queue_t;
+
+
+
+queue_t* open(void){
+	queue_t* newQueue = (queue_t*)malloc(sizeof(queue_t));
+    
+    if (newQueue == NULL) {
+        // Handle memory allocation error if needed
+        //perror("Memory allocation error");
+        return NULL;
+    }
+
+    // Initialize front and back pointers to NULL
+    newQueue->front = NULL;
+    newQueue->back = NULL;
+
+    return newQueue;
+}
+
+void qclose(queue_t* qp){
+	if(qp == NULL){
+		return;
+	}
+
+	while (qp->front != NULL) {
+        // Remove and free the front node until the queue is empty
+        node_t* temp = qp->front;
+        qp->front = qp->front->next;
+        free(temp);
+    }
+
+    // Free the queue structure itself
+    free(qp);
+}
+
+int32_t qput(queue_t *qp, void *elementp){
+    // Create a new node
+    queue_t* newQueue = (queue_t*)malloc(sizeof(queue_t));
+
+    if (newQueue == NULL) {
+        // Handle memory allocation error
+        //perror("Memory allocation error");
+        return -1; // Nonzero value for failure
+    }
+
+    newQueue->data = elementp;
+    newQueue->next = NULL;
+
+    if (qp->back == NULL) {
+        // If the queue is empty, set front and back to the new node
+        qp->front = newQueue;
+        qp->back = newQueue;
+    } else {
+        // Otherwise, update the back pointer and link nodes
+        qp->back->next = newQueue;
+        qp->back = newQueue;
+    }
+
+    return 0; // Return 0 for success
+}
+
+void* qget(queue_t *qp){
+	//check queue isn't null
+	if(qp == NULL){
+		return NULL;
+	}
+
+	frontOrig = qp->front;
+	//Check if the queue is empty, if yes then return null
+	if(frontOrig == NULL){
+		return NULL;
+	}
+	//If the list isn'r empty return the first element and remove it from the queue
+	else{
+		void *firstElement = frontOrig;;
+		qp->front = frontOrig->next;
+		frontOrig->next = NULL;
+		//check if firstElement was the last element in the list
+		if(qp->front == NULL){
+			qp->back = NULL;
+		}
+		return firstElement;
+	}
+}
